56dbe175df6779a4f5a2fbe1db7a8130dd12f289
Brian Starkey <stark3y@gmail.com>
2021-01-14 19:48:24 +0000

    Add AXP192 component(s) and read rails

--- main/main.c
+++ main/main.c
@@ -5,6 +5,9 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "axp192.h"
+#include "i2c_helper.h"
+
 void app_main(void)
 {
 	printf("Hello world!\n");
@@ -25,11 +28,53 @@ void app_main(void)
 
 	printf("Free heap: %d\n", esp_get_free_heap_size());
 
-	for (int i = 10; i >= 0; i--) {
-		printf("Restarting in %d seconds...\n", i);
+	const axp192_t axp = {
+		.read = &i2c_read,
+		.write = &i2c_write,
+	};
+
+	i2c_init();
+
+	// don't axp192_init() because we don't wan't to run the initial commands
+	// axp192_init(&axp);
+
+	// DCDC1: MCU_VDD  (3v3)
+	// DCDC2: Not connected
+	// DCDC3: LCD_BL   (2v8)
+	// LDO1: RTC, non-configured
+	// LDO2: PERI_VDD  (3v3)
+	// LDO3: VIB_MOTOR (2v0)
+	struct rail_entry {
+		const char *name;
+		axp192_rail_t rail;
+		uint16_t millivolts;
+	};
+
+	struct rail_entry rails[] = {
+		{ "DCDC1 (MCU_VDD)",  AXP192_RAIL_DCDC1, 3300 },
+		// DCDC2 not connected
+		{ "DCDC3 (LCD_BL)",   AXP192_RAIL_DCDC3, 2800 },
+		// LDO1 not configurable
+		{ "LDO2 (PERI_VDD)",  AXP192_RAIL_LDO2,  3300 },
+		{ "LDO3 (VIB_MOTOR)", AXP192_RAIL_LDO3,  2000 },
+	};
+
+	for (int i = 0; i < sizeof(rails) / sizeof(rails[0]); i++) {
+		uint16_t millivolts;
+		axp192_err_t err = axp192_get_rail_millivolts(&axp, rails[i].rail, &millivolts);
+		if (err != AXP192_ERROR_OK) {
+			printf("%s: get failed\n", rails[i].name);
+			continue;
+		}
+
+		printf("%s: get %d mV\n", rails[i].name, millivolts);
+	}
+
+	fflush(stdout);
+
+	for ( ; ; ) {
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 	printf("Restarting now.\n");
-	fflush(stdout);
 	esp_restart();
 }
