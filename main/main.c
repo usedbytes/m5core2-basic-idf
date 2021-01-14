90188d0fa55bcbd5ec5e5d3792d76efa1814bc9c
Brian Starkey <stark3y@gmail.com>
2021-01-14 20:55:54 +0000

    main: Setup more AXP192 bits and test LED

--- main/main.c
+++ main/main.c
@@ -8,6 +8,26 @@
 #include "axp192.h"
 #include "i2c_helper.h"
 
+void set_axp192_gpio_012(const axp192_t *axp, int gpio, bool low) {
+	if ((gpio < 0) || (gpio > 2)) {
+		return;
+	}
+
+	uint8_t val = 0;
+	axp192_read_reg(axp, AXP192_GPIO20_SIGNAL_STATUS, &val);
+
+	uint8_t mask = (1 << gpio);
+	if (low) {
+		// Value of 0 activates the NMOS, pulling the pin low
+		val &= ~mask;
+	} else {
+		// Value of 1 sets floating
+		val |= mask;
+	}
+
+	axp192_write_reg(axp, AXP192_GPIO20_SIGNAL_STATUS, val);
+}
+
 void app_main(void)
 {
 	printf("Hello world!\n");
@@ -101,6 +121,102 @@ void app_main(void)
 		float volts;
 		axp192_read(&axp, AXP192_BATTERY_VOLTAGE, &volts);
 		printf("Battery voltage: %.2f volts\n", volts);
+	}
+
+	// AXP192 GPIOs and pins
+	{
+		// GPIO1_CONTROL and GPIO2_CONTROL
+		// (Direct translations from the datasheet)
+		// Bits 7-3: Reserved
+		// Bits 2-0: 000: NMOS open-drain output
+		//           001: Universal input function
+		//           010: PWM1 output, high level is VINT, no pull down less than 100k can be added
+		//           011: Reserved
+		//           100: ADC input
+		//           101: Low output
+		//           11X: Floating
+
+		// Open-drain outputs
+		// GPIO1: LED sink
+		axp192_write_reg(&axp, AXP192_GPIO1_CONTROL, 0);
+		// GPIO2: SPK_EN
+		axp192_write_reg(&axp, AXP192_GPIO2_CONTROL, 0);
+
+		// GPIO[4:3]_CONTROL
+		// (Direct translations from the datasheet)
+		// Bit    7: 1: GPIO function
+		// Bits 6-4: Reserved
+		// Bits 3-2: GPIO4 Function
+		//           00: External charging control
+		//           01: NMOS open drain
+		//           10: GPIO
+		//           11: Reserved
+		// Bits 1-0: GPIO3 Function
+		//           00: External charging control
+		//           01: NMOS open drain
+		//           10: GPIO
+		//           11: ADC Input
+		// Set GPIO4 (LCD_RST) and GPIO3 (NC) to GPIO
+		axp192_write_reg(&axp, AXP192_GPIO40_FUNCTION_CONTROL, (1 << 7) | (2 << 2) | (2 << 0));
+
+		// PEK (power enable key)
+		// Bits 7-6: Boot time
+		//           00: 128 ms
+		//           01: 512 ms
+		//           10: 1 s
+		//           11: 2 s
+		// Bits 5-4: Long press time
+		//           00: 1 s
+		//           01: 1.5 s
+		//           10: 2 s
+		//           11: 2.5 s
+		// Bit 3: Shutdown on long press
+		// Bit 2: PWROK delay
+		//           0: 32 ms
+		//           1: 64 ms
+		// Bit 1-0: Shutdown button duration
+		//           00: 4 s
+		//           01: 6 s
+		//           10: 8 s
+		//           11: 10 s
+		// Copy setting from Arduino (0x4c)
+		axp192_write_reg(&axp, AXP192_PEK, (1 << 6) | (0 << 4) | (1 << 3) | (1 << 2) | (0 << 0));
+
+		// Arduino code enables all ADC channels
+		//axp192_write_reg(&axp, AXP192_ADC_ENABLE_1, 0xff);
+
+		// BATTERY_CHARGE_CONTROL
+		// Bit    7: Backup battery charging enable
+		// Bits 6-5: Backup battery target voltage
+		//           00: 3.1 V
+		//           01: 3.0 V
+		//           10: 3.0 V (duplicated?)
+		//           11: 2.5 V
+		// Bits 4-2: Reserved
+		// Bits 1-0: Backup battery charge current
+		//           00: 50 uA
+		//           01: 100 uA
+		//           10: 200 uA
+		//           11: 400 uA
+		axp192_write_reg(&axp, AXP192_BATTERY_CHARGE_CONTROL, (1 << 7) | (1 << 5) | (0 << 0));
+	}
+
+	// Test LED
+	// Low side is on AXP GPIO1
+	{
+		printf("LED");
+		for (int i = 0; i < 5; i++) {
+			set_axp192_gpio_012(&axp, 1, true);
+			vTaskDelay(300 / portTICK_PERIOD_MS);
+			set_axp192_gpio_012(&axp, 1, false);
+			vTaskDelay(300 / portTICK_PERIOD_MS);
+
+			printf(".");
+			fflush(stdout);
+		}
+		printf("Done.\n");
+		// Leave the LED on
+		set_axp192_gpio_012(&axp, 1, true);
 	}
 
 	// Test vibration
